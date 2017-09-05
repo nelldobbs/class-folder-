68f567d1f0bf83de82605d707d382c0555c64538
nelldobbs <31457899+nelldobbs@users.noreply.github.com>
2017-09-05 19:26:00 -0400

    Add files via upload

---
+++ homeworklesson1_selfportrait/works/wk1_ homework/sketch.js
@@ -0,0 +1,25 @@
+function setup(){
+createCanvas(1024,768);    
+    
+    
+    
+}
+function draw() {
+ background(251,203,204);
+  stroke(255);
+    strokeWeight(10);
+ triangle(612, 500, 512, 300, 412, 500)
+fill(255,13,100,100);
+    fill(56, 186, 182);
+ stroke(255);
+    strokeWeight(10);
+ ellipse(512,230,95,95); 
+ fill(255,13,100,100);
+    fill(56, 186, 182);
+rect(474, 525, 76, 110)
+    fill(56, 186, 182);
+ stroke(255);
+    strokeWeight(10);
+    
+    
+}
